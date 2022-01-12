6cf4158c8a99861707e2e77448c61abb8bce493e
Jeonjueun07 <lovesj2323@naver.com>
2022-01-12 17:30:31 +0900

    Study

--- src/com/daru/s3/ex1/MemberMain.java
+++ src/com/daru/s3/ex1/MemberMain.java
@@ -1,21 +1,31 @@
 package com.daru.s3.ex1;
 
+import java.util.ArrayList;
+
 public class MemberMain {
 
 	public static void main(String[] args) {
 
 		// test, 프로그램 실행
 		MemberData memberData = new MemberData();
-		MemberDTO[] members = memberData.init();
-
-		for (int i = 0; i < members.length; i++) {
-			System.out.println(members[i].getId());
-			System.out.println(members[i].getPw());
-			System.out.println(members[i].getName());
-			System.out.println(members[i].getEmail());
+		ArrayList<MemberDTO> members = memberData.init();
+		
+		//memberData.addMember(members);
+		MemberDTO memberDTO = memberData.removeMember(members);
+		if(memberData != null) {
+			System.out.println("삭제성공");
+		}else {
+			System.out.println("삭제실패");
+		}
+		
+		for (int i = 0; i < members.size(); i++) {
+			System.out.println(members.get(i).getId());
+			System.out.println(members.get(i).getPw());
+			System.out.println(members.get(i).getName());
+			System.out.println(members.get(i).getEmail());
 			System.out.println("===================");
 		}
-
+		
 	}
 
 }
---
+++ src/com/daru/s3/ex2/StudentData.java
@@ -0,0 +1,17 @@
+package com.daru.s3.ex2;
+
+public class StudentData {
+	private String info;
+	
+	public StudentData() {
+		this.info = "name1, 1, 15, 50, 60";
+		this.info = this.info+"name2, 2, 52, 68, 40";
+		this.info = this.info+"name3 , 3, 80,87,75";
+		
+	}
+	
+	//학생의 정보를 파싱해서 총점과평균도 계산후전체출력
+	//학생의 정보 추가
+	//학생의 정보 삭제
+	
+}
