a6ca8ada8059e4798e3d0a581d7c0c459882dcb6
Jeonjueun07 <lovesj2323@naver.com>
2022-01-13 15:11:18 +0900

    Study

--- src/com/daru/s3/ex1/Ex1Main.java
+++ src/com/daru/s3/ex1/Ex1Main.java
@@ -1,9 +1,11 @@
 package com.daru.s3.ex1;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 import com.daru.s3.ex1.member.MemberDTO;
 import com.daru.s3.ex1.member.MemberData;
+import com.daru.s3.ex1.student.StudentDTO;
 import com.daru.s3.ex1.student.StudentData;
 
 public class Ex1Main {
@@ -15,7 +17,8 @@ public class Ex1Main {
 		
 		//회원들의 정보 모음
 		ArrayList<MemberDTO> ar = memberData.init();
-		studentData.addStudent(ar);
+		Map<String, StudentDTO> map = studentData.addStudent(ar);
+		System.out.println(map);
 		
 	}
 
--- src/com/daru/s3/ex1/student/StudentData.java
+++ src/com/daru/s3/ex1/student/StudentData.java
@@ -1,5 +1,6 @@
 package com.daru.s3.ex1.student;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
@@ -8,19 +9,46 @@ import com.daru.s3.ex1.member.MemberDTO;
 
 
 public class StudentData {
-
+	private Scanner sc;
+	
+	public StudentData() {
+		sc = new Scanner(System.in);
+	}
 	
 	public Map<String, StudentDTO> addStudent(List<MemberDTO> ar){
 		//키보드로부터 멤버의 수만큼 성적정보 입력
 		//이정보들을 StudentDTO에 넣어줌
 		//map의 키는 id
-		Scanner sc = new Scanner(System.in);
-		System.out.println("멤버수");
+		HashMap<String, StudentDTO> map = new HashMap<>();
 		
+		for(int i=0;i<ar.size();i++) {
+			StudentDTO studentDTO = new StudentDTO();
+			studentDTO.setId(ar.get(i).getId());
+			studentDTO.setName(ar.get(i).getName());
+			//id
+			//name
+			System.out.println("국어성적입력");
+			studentDTO.setKor(sc.nextInt());
+			System.out.println("영어성적입력");
+			studentDTO.setEng(sc.nextInt());
+			System.out.println("수학성적입력");
+			studentDTO.setMath(sc.nextInt());
+			//총점
+			System.out.println("총점");
+			studentDTO.setTotal(studentDTO.getKor()+studentDTO.getEng()+studentDTO.getMath());
+			//평균
+			System.out.println("평균");
+			studentDTO.setAvg(studentDTO.getTotal()/3.0);
+			//map에 추가
+			map.put(studentDTO.getId(), studentDTO);
+		}
+		
+		
+		HashMap<String, StudentDTO> sid = new HashMap<>();
 		
 		
 	
-		return null;
+		return map;
 	}
 
 }
